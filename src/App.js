9016c8a815a6644170f07339fae9bb0232e65be6
lhw <85226411@qq.com>
2019-02-15 14:46:04 +0800

    20190215 新增左侧导航

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import './static/css/font-awesome.min.css';
 import './App.css';
 import {
   BrowserRouter,
   Route,
-  Link
+  Link,
+  NavLink
 } from 'react-router-dom';
 import Home from './pages/home';
 import Detail from './pages/detail';
@@ -17,22 +19,49 @@ class App extends Component {
         </header>
         <BrowserRouter>
         <div>
-        <div className="App-left">
-            <ul>
-              <li><Link to='/home'>关于</Link></li>
-              <li><Link to='/detail'>主题列表</Link></li>
-            </ul>
-        </div>
-        <div className="App-right">
-        <Route exact path='/' component={Home} />
-        <Route exact path='/home' component={Home} />
-        <Route exact path='/detail' component={Detail} />
-        </div>
+          <div className="App-collapse"><i className="fa fa-angle-double-left"> </i></div>
+          <div className="App-left">
+              <ul>
+                <NavLink to='/home' activeClassName="active"><li><i className="icon"></i>首页</li></NavLink>
+                <li onClick={handleClick}>
+                  <div className="nav-one active"><i className="icon fa fa-caret-down"> </i>一级菜单 </div>
+                  <ul>
+                    <NavLink to='/detail' activeClassName="active"><li>二级菜单</li></NavLink>
+                    <NavLink to='/detail' activeClassName="active"><li>二级菜单</li></NavLink>
+                    <NavLink to='/detail' activeClassName="active"><li>二级菜单</li></NavLink>
+                  </ul>
+                </li>
+                <li onClick={handleClick}>
+                  <div className="nav-one"><i className="icon fa fa-caret-right"> </i>一级菜单 </div>
+                  <ul>
+                    <li><Link to='/detail'>二级菜单</Link></li>
+                    <li><Link to='/detail'>二级菜单</Link></li>
+                    <li><Link to='/detail'>二级菜单</Link></li>
+                  </ul>
+                </li>
+              </ul>
+          </div>
+          <div className="App-right">
+            <Route exact path='/' component={Home} />
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/detail' component={Detail} />
+          </div>
         </div>
         </BrowserRouter>
       </div>
     );
   }
 }
-
+function handleClick(e) {//菜单的下拉收起
+  var classNameStr=e.target.className
+  if(classNameStr.indexOf("nav-one")===0){
+     if(classNameStr.indexOf("active")>0){
+        e.target.className="nav-one";
+        e.target.firstChild.className="icon fa fa-caret-right"
+     }else{
+        e.target.className="nav-one active";
+        e.target.firstChild.className="icon fa fa-caret-down"
+     }
+  };
+}
 export default App;
